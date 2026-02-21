d7e64cceba0bd012ef590346cb1cd47da604d634
Karan Aggarwal <karanagg2005@gmail.com>
2026-02-21 23:29:06 +0530

    Prepare for deployment

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,8 @@ const port = 8080;
 
 
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:3000',
+app.use(cors({ 
+    origin: process.env.CLIENT_URL,
     credentials: true
 }));
 
